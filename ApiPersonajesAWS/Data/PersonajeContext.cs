7b25fa709b44699d680fe85676448df49bf0120a
NazarRom <nazarrom321@gmail.com>
2023-05-24 13:00:02 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Data/PersonajeContext.cs
@@ -0,0 +1,11 @@
+﻿using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonajesAWS.Data
+{
+    public class PersonajeContext : DbContext
+    {
+        public PersonajeContext(DbContextOptions<PersonajeContext> options) : base(options) { }
+        public DbSet<Personaje> Personajes { get; set; }
+    }
+}
---
+++ ApiPersonajesAWS/Repositories/RepositoryPersonajes.cs
@@ -0,0 +1,37 @@
+﻿using ApiPersonajesAWS.Data;
+using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonajesAWS.Repositories
+{
+    public class RepositoryPersonajes
+    {
+        private PersonajeContext context;
+
+        public RepositoryPersonajes(PersonajeContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+           return await this.context.Personajes.ToListAsync();
+        }
+
+        public async Task<Personaje> FindPersonajeAsync(int id)
+        {
+            return await this.context.Personajes.FirstOrDefaultAsync(x => x.IdPersonaje == id);
+        }
+
+        public async Task InsertPersonajeAsync(int id, string nombre, string imagen)
+        {
+            Personaje personaje = new Personaje();
+            personaje.IdPersonaje = id;
+            personaje.Nombre = nombre;
+            personaje.Imagen = imagen;
+            await this.context.Personajes.AddAsync(personaje);
+            await this.context.SaveChangesAsync();
+
+        }
+    }
+}
